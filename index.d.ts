0a7fd5733597e2f8c557d8ae72c2cedbd1458a72
Rootdiv <rootdiv@mail.ru>
2022-10-23 21:51:14 +0300

    fix: typescript template

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,5 @@
 declare module 'dotenv';
-declare module 'html-webpack-plugin';
 declare module 'mini-css-extract-plugin';
-declare module 'postcss-preset-env';
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
